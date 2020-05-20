2a8d535d253ad6e9c434bf06a1dc7356cdd3a749
frstshmn <theskaters75@gmail.com>
2020-05-21 00:41:01 +0300

    Added unique number of exam ticket

--- ticket-mix.js
+++ ticket-mix.js
@@ -1,16 +1,23 @@
 $('#give_tickets').click(function(event) {
-	let ticket_numbers = [];
-	$('.list-item').remove();
+	let list = $('#persons').val().split('\n');
 
-	$.each($('#persons').val(), function(index, value) {
-		$('#result').append('<p class="list-item">'+value+'<p>')
+	let ticket_numbers = [];
+
+	let l = list.length;
+	while(ticket_numbers.length < l){
+		//alert(ticket_numbers.length + '' + l);
+		let r = getRndInteger(1, l)
+		if(ticket_numbers.indexOf(r) === -1) 
+			ticket_numbers.push(r);
+	}
+
+	$('.list-item').remove();
+	$.each(list, function(index, value) {
+		
+		$('#result').append('<p class="list-item"><b>'+ ticket_numbers[index] +'</b> - '+ value +'<p>')
 	});
 });
 
-function randomInt(min, max) {
-	return min + Math.floor((max - min) * Math.random());
-}
-
-function randomFloat(min, max) {
-    return min + (max - min) * Math.random();
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
