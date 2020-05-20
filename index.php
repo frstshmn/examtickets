024b676c4a49a11cca4565dfd6ac442d5ad2880f
frstshmn <theskaters75@gmail.com>
2020-05-20 21:03:33 +0300

    Adding metadata

--- index.php
+++ index.php
@@ -1,11 +1,40 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
 		<title>Видання білетів</title>
+
+	    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700;800&display=swap" rel="stylesheet">
+
+	    <!-- Bootstrap CSS CDN -->
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+	    <!-- Our Custom CSS -->
+	    <link rel="stylesheet" href="octo_front/styles/style.css">
+	    <link rel="stylesheet" href="octo_front/styles/shape.css">
+
+	    <!-- Font Awesome JS -->
+	    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+	    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
+		<!-- Vue.js -->
+		<script src="https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.js"></script>
+		
+		<!-- Axios -->
+		<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 	</head>
 	<body>
 		<div class="container">
-			
+			<div class="row mt-5">
+				<div class="col-4">
+					<div class="form-group">
+						<textarea id="persons"></textarea>
+					</div>
+				</div>
+			</div>
 		</div>
 	</body>
 </html>
