51973a90d9bd8c48b80afff2d1e276b3c0bae2b0
frstshmn <theskaters75@gmail.com>
2020-05-21 01:07:07 +0300

    Adding styles

--- index.php
+++ index.php
@@ -18,21 +18,33 @@
 	    <!-- Font Awesome JS -->
 	    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
 	</head>
-	<body>
+	<body class="font">
 		<div class="container">
-			<div class="row mt-5">
+				
+			<div class="row mt-3">
+				<div class="col-12 text-center">
+					<h1>Сервіс видачі екзаменаційних білетів<h1>
+				</div>
+				<div class="col-12 text-center mb-5">
+					<small class="text-muted">та насолоди муками студентів на екзаменах</small>
+				</div>
 				<div class="col-md-4 col-xs-12">
 					<div class="form-group">
-						<textarea id="persons" class="form-control"></textarea>
+						<textarea id="persons" class="form-control" rows="10"></textarea>
 					</div>
 				</div>
 				<div class="col-md-4 col-xs-12 text-center">
-					<button class="btn button btn-dark">Роздати білети</button>
+					<p class="text-left text-muted text-monospace">1. Введіть у поле праворуч список осіб, яким потрібно роздати білет. Кожну нову особу вводьте з нового рядка, натискаючи клавішу <kbd>Enter</kbd></p>
+					<p class="text-left text-muted text-monospace">2. Натисніть кнопку "Роздати білети".</p>
+					<button class="btn button btn-dark py-2 px-4 border-0 my-2" id="give_tickets">Роздати білети</button>
+					<p class="text-left text-muted text-monospace">3. Вуаля:)</p>
 				</div>
-				<div class="col-md-4 col-xs-12">
+				<div class="col-md-4 col-xs-12" id="result">
 					
 				</div>
 			</div>
 		</div>
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+		<script type="text/javascript" src="ticket-mix.js"></script>
 	</body>
 </html>
